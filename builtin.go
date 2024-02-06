9345b1a4223931976bb6c88925b79ec7905b663e
wangfeng <wangfengxy@sina.cn>
2024-02-06 14:59:20 +0800

    从engine迁移成交数据相关时间常量

--- builtin.go
+++ builtin.go
@@ -1,1 +1,9 @@
 package exchange
+
+// 历史成交数据 historical transaction data
+const (
+	HistoricalTransactionDataFirstTime        = "09:25" // 第一个时间
+	HistoricalTransactionDataStartTime        = "09:30" // 开盘时间
+	HistoricalTransactionDataFinalBiddingTime = "14:57" // 尾盘集合竞价时间
+	HistoricalTransactionDataLastTime         = "15:00" // 最后一个时间
+)
