838e078bedda083fff737ef5abdcdd662c52b549
wangfeng <wangfengxy@sina.cn>
2024-05-20 07:28:48 +0800

    调整私有变量名为驼峰命名规则

--- cache/cache.go
+++ cache/cache.go
@@ -6,10 +6,14 @@ import (
 	"sync"
 )
 
+const (
+	defaultQuant1xDataPath = "~/.quant1x" // 默认的数据路径
+)
+
 var (
-	__global_cache_once   sync.Once      // 懒加载锁
-	__global_cache_path   = "~/.quant1x" // 数据根路径
-	__once_temporary_path = "~/.quant1x" // 临时路径
+	globalCacheOnce   sync.Once                // 懒加载锁
+	globalCachePath   = defaultQuant1xDataPath // 数据根路径
+	onceTemporaryPath = defaultQuant1xDataPath // 临时路径
 )
 
 func initPath(path string) {
@@ -17,29 +21,29 @@ func initPath(path string) {
 	if err != nil {
 		logger.Fatalf("%+v", err)
 	}
-	__once_temporary_path = path
-	__global_cache_path = finalPath
+	onceTemporaryPath = path
+	globalCachePath = finalPath
 }
 
 // InitCachePath 公开给外部调用的初始化路径的函数
 //
 //	lazyInit和InitCachePath两者只能真正被调用一次
 func InitCachePath(path string) {
-	__global_cache_once.Do(func() {
-		__once_temporary_path = path
+	globalCacheOnce.Do(func() {
+		onceTemporaryPath = path
 		initPath(path)
 	})
 }
 
 // 默认的初始化路径
 func lazyInit() {
-	initPath(__once_temporary_path)
+	initPath(onceTemporaryPath)
 }
 
 // DefaultCachePath 数据缓存的根路径
 func DefaultCachePath() string {
-	__global_cache_once.Do(lazyInit)
-	return __global_cache_path
+	globalCacheOnce.Do(lazyInit)
+	return globalCachePath
 }
 
 // GetMetaPath 元数据缓存路径
