f6be385612f2436821eb4e9596ff95e3e5e8bcf7
wangfeng <wangfengxy@sina.cn>
2025-09-14 12:35:56 +0800

    适配x基础工具包

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -9,7 +9,4 @@ func TestCacheInit(t *testing.T) {
 	InitCachePath("~/.quant2x")
 	path := GetMetaPath()
 	fmt.Println(path)
-	InitCachePath("~/.quant3x")
-	path = GetMetaPath()
-	fmt.Println(path)
 }
