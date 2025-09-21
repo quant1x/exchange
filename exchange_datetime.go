b2a64320f4f503a8979f59b37a06aef4a0a0cfd7
wangfeng <wangfengxy@sina.cn>
2025-09-22 07:21:43 +0800

    修订构造交易日的函数

--- exchange_datetime.go
+++ exchange_datetime.go
@@ -1,8 +1,9 @@
 package exchange
 
+// 交易日, 固定格式YYYY-MM-DD
 type TradingDay string
 
-func NewTradeDate(date string) TradingDay {
+func NewTradingDay(date string) TradingDay {
 	date = FixTradeDate(date)
 	return TradingDay(date)
 }
